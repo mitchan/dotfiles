af29f445672c2e82fad15d0af03b0810ad558cdc
Francesco Casetta <francesco.casetta@gmail.com>
2024-10-09 09:13:01 +0200

    wezterm: added tokio night theme

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,29 +1,13 @@
--- Pull in the wezterm API
 local wezterm = require("wezterm")
 
--- This will hold the configuration.
 local config = wezterm.config_builder()
 
--- This is where you actually apply your config choices
-
--- For example, changing the color scheme:
-config.color_scheme = "catppuccin-mocha"
 config.enable_tab_bar = false
-config.window_background_image_hsb = {
-	-- Darken the background image by reducing it to 1/3rd
-	brightness = 0.3,
 
-	-- You can adjust the hue by scaling its value.
-	-- a multiplier of 1.0 leaves the value unchanged.
-	hue = 1.0,
+config.color_scheme = "Tokyo Night"
+-- config.color_scheme = "catppuccin-mocha"
 
-	-- You can adjust the saturation also.
-	saturation = 1.0,
-}
-
--- fonts
 config.font = wezterm.font("MesloLGS Nerd Font")
 config.font_size = 16.0
 
--- and finally, return the configuration to wezterm
 return config
