a29b25f78fbf0ac3bf114e0c77eeb997273b7ce1
Francesco Casetta <francesco.casetta@gmail.com>
2025-01-15 08:46:06 +0100

    wezterm

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -13,11 +13,10 @@ config.enable_tab_bar = true
 -- window
 config.window_background_opacity = 0.8
 config.macos_window_background_blur = 20
-config.window_decorations = "RESIZE"
+-- config.window_decorations = "RESIZE"
 
 -- fonts
-config.font = wezterm.font_with_fallback({ "JetBrains Mono", "MesloLGS Nerd Font" })
-config.font_size = 18.0
-config.line_height = 1.2
+-- config.font = wezterm.font_with_fallback({ "JetBrains Mono", "MesloLGS Nerd Font" })
+config.font_size = 16.0
 
 return config
