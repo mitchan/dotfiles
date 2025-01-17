dd44f5d96c75824e756553a3d7db84b6f48bfc51
Francesco Casetta <francesco.casetta@gmail.com>
2025-01-17 08:55:40 +0100

    nvim theme

--- nvim/.config/nvim/lua/config/plugins/theme.lua
+++ nvim/.config/nvim/lua/config/plugins/theme.lua
@@ -1,21 +1,41 @@
+-- local theme = 'catppuccin'
+local theme = 'tokyonight'
+
+local isCatppuccin = theme == 'catppuccin'
+local isTokyo = theme == 'tokyonight'
+
 return {
   {
     'folke/tokyonight.nvim',
     lazy = false,
-    priority = 1000,
+    priority = isTokyo and 1000 or 50,
     opts = {
       transparent = false,
     },
+    config = function(_, opts)
+      require('tokyonight').setup(opts)
+
+      if isTokyo then
+        vim.cmd.colorscheme 'tokyonight'
+      end
+    end,
   },
 
   {
     'catppuccin/nvim',
     lazy = false,
     name = 'catppuccin',
-    priority = 1000,
+    priority = isCatppuccin and 1000 or 50,
     opts = {
       flavour = 'frappe',
       transparent_background = false,
     },
+    config = function(_, opts)
+      require('catppuccin').setup(opts)
+
+      if isCatppuccin then
+        vim.cmd.colorscheme 'catppuccin'
+      end
+    end,
   },
 }
