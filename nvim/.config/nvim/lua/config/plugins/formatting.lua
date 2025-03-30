7ae71a1721f407f4e1c771830507ad5b7327f226
Francesco Casetta <francesco.casetta@gmail.com>
2025-03-30 10:49:55 +0200

    feat: blink

--- nvim/.config/nvim/lua/config/plugins/formatting.lua
+++ nvim/.config/nvim/lua/config/plugins/formatting.lua
@@ -2,30 +2,19 @@ local function fmtPrettier()
   return { 'prettierd', 'prettier', stop_after_first = true }
 end
 
-return { -- Autoformat
+return {
   'stevearc/conform.nvim',
   event = { 'BufWritePre' },
   cmd = { 'ConformInfo' },
-  keys = {
-    {
-      '<leader>f',
-      function()
-        require('conform').format { async = true, lsp_fallback = true }
-      end,
-      mode = '',
-      desc = '[F]ormat buffer',
-    },
-  },
   opts = {
     notify_on_error = false,
     format_on_save = function(bufnr)
-      -- Disable "format_on_save lsp_fallback" for languages that don't
-      -- have a well standardized coding style. You can add additional
-      -- languages here or re-enable it for the disabled ones.
-      local disable_filetypes = { c = true, cpp = true }
+      -- example for preventing format for specific file types
+      -- local disable_filetypes = { c = true, cpp = true }
       return {
         timeout_ms = 500,
-        lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
+        lsp_fallback = 'fallback',
+        -- lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
       }
     end,
     formatters_by_ft = {
@@ -37,7 +26,7 @@ return { -- Autoformat
       lua = { 'stylua' },
       markdown = fmtPrettier(),
       php = { 'php-cs-fixer' },
-      python = { 'black' },
+      python = { 'isort', 'black' },
       typescript = fmtPrettier(),
       typescriptreact = fmtPrettier(),
       vue = fmtPrettier(),
