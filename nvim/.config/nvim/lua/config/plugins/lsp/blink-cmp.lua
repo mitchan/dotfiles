d5734922fd78cac5c937789105eb7968af720d30
Francesco Casetta <francesco.casetta@gmail.com>
2025-04-05 11:22:09 +0200

    nvim: fzf-lua

--- nvim/.config/nvim/lua/config/plugins/lsp/blink-cmp.lua
+++ nvim/.config/nvim/lua/config/plugins/lsp/blink-cmp.lua
@@ -14,7 +14,13 @@ return {
       nerd_font_variant = 'mono',
     },
 
-    completion = { documentation = { auto_show = true }, trigger = { show_on_keyword = false } },
+    completion = {
+      documentation = { auto_show = true },
+      trigger = {
+        show_on_keyword = false,
+        show_on_trigger_character = false,
+      },
+    },
 
     sources = {
       default = { 'lsp', 'path', 'snippets', 'buffer' },
