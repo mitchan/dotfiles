2237a00c29c673937d4d77d2f6e42ca64bc79c38
Francesco Casetta <francesco.casetta@gmail.com>
2025-08-29 08:26:07 +0200

    nvim: options

--- .config/nvim/lua/config/core/options.lua
+++ .config/nvim/lua/config/core/options.lua
@@ -1,19 +1,39 @@
+vim.opt.cursorline = true
 vim.opt.number = true
 vim.opt.relativenumber = true
+
+-- visual settings
 vim.opt.colorcolumn = '100'
+vim.opt.signcolumn = 'yes'
+vim.opt.termguicolors = true
+vim.opt.showmode = false
+vim.opt.showmatch = true
+vim.opt.matchtime = 2
+
+-- files
+vim.opt.autoread = true
+vim.opt.swapfile = false
+vim.opt.undofile = true
+
+-- Set undo directory and ensure it exists
+local undodir = os.getenv 'HOME' .. '/.vim/undodir'
+vim.opt.undodir = vim.fn.expand(undodir)
+local undodir_path = vim.fn.expand(undodir)
+if vim.fn.isdirectory(undodir_path) == 0 then
+  vim.fn.mkdir(undodir_path, 'p')
+end
 
 vim.opt.mouse = 'a'
+vim.opt.encoding = 'UTF-8'
+vim.opt.wildmenu = true -- Enable command-line completion menu
+vim.opt.wildmode = 'longest:full,full' -- Completion mode for command-line
+vim.opt.wildignorecase = true -- Case-insensitive tab completion in commands
 
 vim.schedule(function()
   vim.opt.clipboard = 'unnamedplus'
 end)
 
 vim.opt.breakindent = true
-
-vim.opt.swapfile = false
-vim.opt.showmode = false
-vim.opt.undofile = true
-vim.opt.undodir = os.getenv 'HOME' .. '/.vim/undodir'
 
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
@@ -29,9 +49,10 @@ vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 
 vim.opt.inccommand = 'split'
 
-vim.opt.cursorline = true
-
 vim.opt.scrolloff = 10
+vim.opt.sidescrolloff = 8
+vim.opt.wrap = false
+vim.opt.cmdheight = 1
 
 -- tabs & indentation
 vim.opt.tabstop = 2 -- 2 spaces for tabs (prettier default)
@@ -39,14 +60,11 @@ vim.opt.shiftwidth = 2 -- 2 spaces for indent width
 vim.opt.expandtab = true -- expand tab to spaces
 vim.opt.autoindent = true -- copy indent from current line when starting new ones
 
--- turn on termguicolors for tokyonight colorscheme to work
--- (have to use iterm2 or any other true color terminal)
-vim.opt.termguicolors = true
-vim.opt.background = 'dark' -- colorschemes that can be light or dark will be made dark
-vim.opt.signcolumn = 'yes' -- show sign column so that text doesn't shift
-
 -- backspace
 vim.opt.backspace = 'indent,eol,start' -- allow backspace on indent, end of line or insert mode start position
 
 -- Set the default border for all floating windows
 vim.opt.winborder = 'rounded'
+
+vim.opt.grepprg = 'rg --vimgrep' -- Use ripgrep if available
+vim.opt.grepformat = '%f:%l:%c:%m' -- filename, line number, column, content
