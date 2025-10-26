6a75c6f302fe5ff56390f11ac2367c25ae750359
Francesco Casetta <francesco.casetta@gmail.com>
2025-10-26 17:43:29 +0100

    feat: lualine

--- config/.config/nvim/lua/config/plugins/mini.lua
+++ config/.config/nvim/lua/config/plugins/mini.lua
@@ -32,20 +32,20 @@ return {
     end,
   },
 
-  {
-    'nvim-mini/mini.statusline',
-    version = false,
-    opts = { use_icons = vim.g.have_nerd_font },
-    config = function(_, opts)
-      local statusline = require 'mini.statusline'
-      statusline.setup(opts)
-
-      ---@diagnostic disable-next-line: duplicate-set-field
-      statusline.section_location = function()
-        return '%2l:%-2v'
-      end
-    end,
-  },
+  -- {
+  --   'nvim-mini/mini.statusline',
+  --   version = false,
+  --   opts = { use_icons = vim.g.have_nerd_font },
+  --   config = function(_, opts)
+  --     local statusline = require 'mini.statusline'
+  --     statusline.setup(opts)
+  --
+  --     ---@diagnostic disable-next-line: duplicate-set-field
+  --     statusline.section_location = function()
+  --       return '%2l:%-2v'
+  --     end
+  --   end,
+  -- },
 
   {
     'nvim-mini/mini.surround',
